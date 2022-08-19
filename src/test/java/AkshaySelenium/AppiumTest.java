55797eae2bef55fc95f9a976b14c1abcc17c9641
Mrakshay <caakshay91@gmail.com>
2022-08-19 12:57:59 +0530

    first commit

---
+++ src/test/java/AkshaySelenium/AppiumTest.java
@@ -0,0 +1,17 @@
+package AkshaySelenium;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPandroid()
+	{
+		System.out.println("NativeAPandroid");
+	}
+    @Test
+    public void IOSApps()
+    {
+    	System.out.println("IOSApps");
+
+}
+}
---
+++ src/test/java/AkshaySelenium/RESTAPITest.java
@@ -0,0 +1,17 @@
+package AkshaySelenium;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void posJira()
+	{
+		System.out.println("Jira");
+	}
+    @Test
+    public void deleteTwitter()
+    {
+    	System.out.println("deleteTwitter");
+    }
+
+}
